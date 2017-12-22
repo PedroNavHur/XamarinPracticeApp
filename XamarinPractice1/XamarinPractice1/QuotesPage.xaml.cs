163d69a41a23ecf8da0d2137978cfc8e2e349f12
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-21 18:21:45 -0600

    Quotes

--- XamarinPractice1/XamarinPractice1/QuotesPage.xaml.cs
+++ XamarinPractice1/XamarinPractice1/QuotesPage.xaml.cs
@@ -9,9 +9,21 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinPractice1
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
 	public partial class QuotesPage : ContentPage
 	{
+        string[] quotes = {
+            "Don't cry because it's over, smile because it happened.",
+            "Be yourself; everyone else is already taken.",
+            "So many books, so little time.",
+            "A room without books is like a body without a soul.",
+            "You only live once, but if you do it right, once is enough.",
+            "Be the change that you wish to see in the world."
+        };
+        
+
+
 		public QuotesPage ()
 		{
 			InitializeComponent ();
