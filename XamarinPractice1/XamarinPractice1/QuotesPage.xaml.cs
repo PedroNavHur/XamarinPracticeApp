098e2a4f72b3916c04b761bc7821dea893bcdf82
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-21 19:31:52 -0600

    Change Quote Button

--- XamarinPractice1/XamarinPractice1/QuotesPage.xaml.cs
+++ XamarinPractice1/XamarinPractice1/QuotesPage.xaml.cs
@@ -11,8 +11,14 @@ namespace XamarinPractice1
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
 
-	public partial class QuotesPage : ContentPage
-	{
+    public partial class QuotesPage : ContentPage
+    {
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+        }
+
         string[] quotes = {
             "Don't cry because it's over, smile because it happened.",
             "Be yourself; everyone else is already taken.",
@@ -21,12 +27,15 @@ namespace XamarinPractice1
             "You only live once, but if you do it right, once is enough.",
             "Be the change that you wish to see in the world."
         };
-        
 
+        int i = 0;
+        void NextButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (i == quotes.Length)
+                i = 0;
 
-		public QuotesPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+            label.Text = quotes[i];
+            i++;
+        }
+    }
 }
