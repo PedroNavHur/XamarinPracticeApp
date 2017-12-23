d7f7bd485bc1eddff7bb07bd1cdd17e38e395dde
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2017-12-22 22:13:25 -0600

    Stack Layout Exercise 1

--- XamarinPractice1/XamarinPractice1/App.xaml.cs
+++ XamarinPractice1/XamarinPractice1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,28 +8,31 @@ using Xamarin.Forms;
 
 namespace XamarinPractice1
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new QuotesPage();
-		}
+            MainPage = new StackLayoutExercise1();
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
